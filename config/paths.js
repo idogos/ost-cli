15ad5cf405284776494bd81c4fc90ac30c76898f
https://github.com/Leotw <1123978215@qq.com>
2018-12-05 18:24:52 +0800

    update:配置文件

--- config/paths.js
+++ config/paths.js
@@ -3,16 +3,18 @@ const path = require('path');
 const fs = require('fs');
 
 const env = require('./env');
+const {local} = require('./test');
 const indefiniteTmpPath = require('../lib/name');
 
 const basePath = fs.realpathSync(process.cwd());
 
-const templateUrl = 'Ironsub/react-ost';
+const templateUrl = 'Ironsub/react-cavalier';
 const tmpPath = path.resolve(basePath, path.join('.', `.${indefiniteTmpPath}`));
+
 const generatorPath = (_env) => {
   let buildPath = null;
   if(_env === env.local) {
-    buildPath = 'build'
+    buildPath = local.path;
   } else if(_env === env.prod) {
     buildPath = '.';
   }
---
+++ config/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  local: {
+    appName: 'my-test-local',
+    templateName: 'react-cavalier',
+    path: 'build'
+  }
+};
