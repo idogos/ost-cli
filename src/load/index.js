5be30e513bc9ccd73f3e1a4af18e605921d93c1c
https://github.com/Leotw <1123978215@qq.com>
2019-01-16 00:23:20 +0800

    update:下载方式更新

--- src/load/index.js
+++ src/load/index.js
@@ -2,7 +2,6 @@ const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 const ora = require('ora');
-const spinner = ora(`loading...\n`);
 
 const scaffolds = require('../../config/base').scaffolds;
 const downloadGitHub = require('./downloadGitHub');
@@ -10,9 +9,11 @@ const downloadNpm = require('./downloadNpm');
 const basePath = fs.realpathSync(process.cwd());
 const savePath = path.resolve(basePath, path.join('.', process.env.NODE_ENV === 'local'? 'build' : ''));
 
+const spinner = ora(`loading...\n`);
+
 async function load(directoryName = null) {
   shell.rm('-rf', savePath);
-  const temName = process.env.TEMPLATE;
+  const temName = process.env.TEMPLATE_NAME;
   const { channel, path, config } = scaffolds[temName];
   spinner.start('loading...\n');
   switch (channel) {
