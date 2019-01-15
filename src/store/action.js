e28f4c966916513574082195dc560cadf51deca2
https://github.com/Leotw <1123978215@qq.com>
2019-01-16 00:24:02 +0800

    update:全局变量封装

--- src/store/action.js
+++ src/store/action.js
@@ -1,3 +1,21 @@
+// 获取样板名
 global.__GET_TEMPLATE_NAME__ = function() {
   return process.env.TEMPLATE_NAME;
-}
+};
+
+// 设置样板名
+global.__SET_TEMPLATE_NAME__ = function(tn) {
+  process.env.TEMPLATE_NAME = tn;
+  return tn;
+};
+
+// 获取自定义项目名
+global.__GET_APP_NAME__ = function() {
+  return process.env.APP_NAME;
+};
+
+// 设置自定义项目名
+global.__SET_APP_NAME__ = function(an) {
+  process.env.APP_NAME = an;
+  return an;
+};
---
+++ src/store/model.js
@@ -0,0 +1,3 @@
+process.env.TEMPLATE_NAME = ''; // 客户终端选取base.json里哪个脚手架
+process.env.APP_NAME = ''; // 客户终端开发者自定义项目名
+global.SPINNER = null; // ora实例
