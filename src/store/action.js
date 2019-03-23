d04ae058986b71132d2a884de9d98483143503f8
https://github.com/Leotw <1123978215@qq.com>
2019-03-24 00:04:23 +0800

    update:新增生成项目的物理路经存储

--- src/store/action.js
+++ src/store/action.js
@@ -1,3 +1,4 @@
+const hash = require('../utils/hash');
 // 获取样板名
 global.__GET_TEMPLATE_NAME__ = function() {
   return process.env.TEMPLATE_NAME;
@@ -16,6 +17,18 @@ global.__GET_APP_NAME__ = function() {
 
 // 设置自定义项目名
 global.__SET_APP_NAME__ = function(an) {
+  if(!an) an = 'app-' + hash();
   process.env.APP_NAME = an;
   return an;
+};
+
+// 设置项目存放地址
+global.__SET_APP_PATH__ = function(ap) {
+  process.env.APP_PATH = ap;
+  return ap;
+};
+
+// 获取项目存放地址
+global.__GET_APP_PATH__ = function() {
+  return process.env.APP_PATH;
 };
--- src/store/model.js
+++ src/store/model.js
@@ -1,11 +1,13 @@
 process.env.TEMPLATE_NAME = ''; // 客户终端选取base.json里哪个脚手架
 process.env.APP_NAME = ''; // 客户终端开发者自定义项目名
+process.env.APP_PATH = ''; // 项目存放路经
 /**
  * 所有的状态码
  * @private
  */
 global.__ERROR_STATUS__ = {
   ['write-error']: '10001@Write error',
-  ['download-npm-fail']: '10002@Download npm fail',
-  ['download-github-fail']: '10003@Download github fail'
+  ['download-npm-fail']: '20002@Download npm fail',
+  ['download-github-fail']: '20003@Download github fail',
+  ['download-not-available']: '20004@Not available load channel'
 };
