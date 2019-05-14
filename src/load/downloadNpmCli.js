a3a2507facfc50f99e99c1d78a376ed73456d48c
https://github.com/Leotw <1123978215@qq.com>
2019-05-15 00:28:33 +0800

    update:修复路经问题

--- src/load/downloadNpmCli.js
+++ src/load/downloadNpmCli.js
@@ -1,26 +1,21 @@
 const shell = require('shelljs');
-const paths = require('../utils/paths');
 const path = require('path');
 /**
  *
  * @param command Linux命令
  * @param savePath 项目保存路经（不含项目名）
  * @param directoryName 项目名
+ * @param join 是否进行路经的拼接（针对测试环境的build目录）
  * @returns {Promise<any>}
  */
-function downloadNpmCli(command, savePath, directoryName = null) {
-  const finalAppPath = `${path.basename(savePath)}/${directoryName}`;
+function downloadNpmCli(command, savePath, directoryName = null, join = false) {
+  const finalAppPath = join ? `${path.basename(savePath)}/${directoryName}` : directoryName;
+  console.log();
+  console.log('finalAppPath:', finalAppPath);
+  console.log();
   const finalPath = `${savePath}/${directoryName}`;
   return new Promise((resolve, reject) => {
     shell.exec(`${command} ${finalAppPath}`, { async: true }, function(code, a, b, c, d) {
-      console.log('-----');
-      console.log('a:', a);
-      console.log('-----');
-      console.log('b:', b);
-      console.log('-----');
-      console.log('c:', c);
-      console.log('-----');
-      console.log('d:', d);
       if(+code !== 0) {
         reject({
           __MSG__: __ERROR_STATUS__['download-npmCli-fail'],
