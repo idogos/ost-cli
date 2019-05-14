524b04fe9b9f0ac2fa8bff35f38b8a5a0aa37818
https://github.com/Leotw <1123978215@qq.com>
2019-05-15 00:28:59 +0800

    update:添加下载前置条件判定

--- src/load/index.js
+++ src/load/index.js
@@ -1,7 +1,8 @@
-const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
 const ora = require('ora');
+const chalk = require('chalk');
+const system = require('../utils/system');
 
 const scaffolds = require('../../config/base').scaffolds;
 const downloadGitHub = require('./downloadGitHub');
@@ -13,6 +14,15 @@ const savePath = path.resolve(basePath, path.join('.', process.env.NODE_ENV === 
 global.spinner = ora(`loading...\n`);
 
 async function load(directoryName = null) {
+  const statusObj = await prepose();
+  let useStatic;
+  if(!statusObj.net) {
+    console.log(chalk.red('当前设备似乎无法访问外部网址'));
+    return;
+  }
+  if(/window/.test(statusObj.system)) {
+    useStatic = true;
+  }
   const temName = process.env.TEMPLATE_NAME;
   const { channel, path, config } = scaffolds[temName];
   let finalSavePath;
@@ -20,14 +30,27 @@ async function load(directoryName = null) {
   switch (channel) {
     case 'github':
       const branch = config['git-branch'];
-      finalSavePath = await downloadGitHub(`${path}${branch ? '#' + branch : ''}`, `${savePath}${directoryName ? '/' + directoryName : ''}`);
+      finalSavePath = await downloadGitHub(
+        `${path}${branch ? '#' + branch : ''}`,
+        `${savePath}${directoryName ? '/' + directoryName : ''}`,
+        useStatic
+      );
       break;
     case 'npm':
-      finalSavePath = await downloadNpm(path, savePath, directoryName);
+      finalSavePath = await downloadNpm(
+        path,
+        savePath,
+        directoryName
+      );
       break;
     case 'npm-cli':
       const exec = config.exec.replace(/{{appName}}/, '');
-      finalSavePath = await downloadNpmCli(exec, savePath, directoryName);
+      finalSavePath = await downloadNpmCli(
+        exec,
+        savePath,
+        directoryName,
+        process.env.NODE_ENV === 'local'
+      );
       break;
     default:
       await Promise.reject({
@@ -40,5 +63,13 @@ async function load(directoryName = null) {
   spinner.succeed('succeed');
 }
 
+async function prepose() {
+  const { getSystem, getNetStatus } = system;
+  const netStatus = await getNetStatus('www.baidu.com');
+  return Promise.resolve({
+    system: getSystem(),
+    net: netStatus
+  });
+}
 module.exports = load;
 
