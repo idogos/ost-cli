de111deb13ee26e099ca2b83c23ae07b4db3e622
https://github.com/Leotw <1123978215@qq.com>
2019-05-15 00:29:19 +0800

    update:添加通过静态资源下载

--- src/load/downloadGitHub.js
+++ src/load/downloadGitHub.js
@@ -1,13 +1,22 @@
 const download = require('download-git-repo');
 const shell = require('shelljs');
+const open = require('open');
+const STATIC_URL = 'https://www.baidu.com/';
 
-function downloadGitHub(url, savePath) {
-  console.log();
-  console.log(savePath);
-  console.log();
+function downloadGitHub(url, savePath, useStatic) {
   const finalPath = `${savePath}`;
   shell.rm('-rf', finalPath);
   return new Promise((resolve, reject) => {
+    useStatic ? open(STATIC_URL)
+      .then(openStatus => {
+        resolve('Download by open browser');
+      })
+      .catch(err => {
+        reject({
+          __MSG__: __ERROR_STATUS__['open-browser-fail'],
+          err
+        });
+      }) :
     download(url, finalPath, null, function(err) {
       if(err) {
         reject({
